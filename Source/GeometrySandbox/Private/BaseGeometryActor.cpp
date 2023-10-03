ef9c3e4f446675a4fc390531d8f20bbb7365a41f
DDRaccoon <115973064+DDRaccoon@users.noreply.github.com>
2023-10-04 00:33:53 +0800

    add_0~10

--- Source/GeometrySandbox/Private/BaseGeometryActor.cpp
+++ Source/GeometrySandbox/Private/BaseGeometryActor.cpp
@@ -3,12 +3,16 @@
 
 #include "BaseGeometryActor.h"
 
+DEFINE_LOG_CATEGORY_STATIC(LogBaseGeometry, All,All);
+
 // Sets default values
 ABaseGeometryActor::ABaseGeometryActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	BaseMesh=CreateDefaultSubobject<UStaticMeshComponent>("BaseMesh");
+	SetRootComponent(BaseMesh);
 }
 
 // Called when the game starts or when spawned
@@ -16,10 +20,11 @@ void ABaseGeometryActor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp,Display,TEXT("一条信息"));
-	UE_LOG(LogTemp,Error,TEXT("一条信息"));
-	UE_LOG(LogTemp,Warning,TEXT("一条信息"));
+	InitialLocation=GetActorLocation();
 	
+	//printStringTypes();
+	//printTypes();
+	printTransform();
 }
 
 // Called every frame
@@ -27,5 +32,60 @@ void ABaseGeometryActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	FVector CurrentLocation =GetActorLocation();
+	float time=GetWorld()->GetTimeSeconds();
+	CurrentLocation.Z=InitialLocation.Z+Amplitude*FMath::Sin(Frequency*time);
+	
+	SetActorLocation(CurrentLocation);
 }
 
+void ABaseGeometryActor::printStringTypes()
+{
+	//字符串
+	FString Name= "Chen SiCheng";
+	UE_LOG(LogBaseGeometry,Display,TEXT("Name:%s"),*Name);
+	
+	FString WeaponsNumStr = "Weapons num = " + FString::FromInt(WeaponsNum);
+	FString HealthStr = "Health = " + FString::SanitizeFloat(Health);
+	FString IsDeadStr = "Is dead = " + FString(IsDead?"true":"fa");
+
+	FString Stat = FString::Printf(TEXT("\n All Stat \n %s %s %s "),*WeaponsNumStr,*HealthStr,*IsDeadStr);
+	UE_LOG(LogBaseGeometry,Warning,TEXT("%s"),*Stat);
+
+	GEngine->AddOnScreenDebugMessage(-1,3.0f,FColor::Red,Name);
+	GEngine->AddOnScreenDebugMessage(-1,5.0f,FColor::Green,Stat,true,FVector2D(1.5F,1.5F));
+	
+	//日志
+	UE_LOG(LogBaseGeometry,Error,TEXT("你谁啊"));
+	UE_LOG(LogBaseGeometry,Display,TEXT("一条信息"));
+	UE_LOG(LogBaseGeometry,Error,TEXT("一条信息"));
+	UE_LOG(LogBaseGeometry,Warning,TEXT("一条信息"));
+}
+
+void ABaseGeometryActor::printTypes()
+{
+	UE_LOG(LogBaseGeometry,Warning,TEXT("Actor name %s"),*GetName());
+	UE_LOG(LogBaseGeometry, Warning,TEXT("Weapons Num: num: &d,kills num:%i"),WeaponsNum,KillsNum);
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Health: %f"), Health);
+	//UE_LOG(LogTemp, Display, TEXT("Health: %.2f"), Health);
+	UE_LOG(LogBaseGeometry, Warning, TEXT("IsDead:%d"), IsDead);
+	UE_LOG(LogBaseGeometry, Warning, TEXT("HasWeapon:%d"), static_cast<int>(HasWeapon));
+}
+
+void ABaseGeometryActor::printTransform()
+{
+	//变换
+	FTransform Transform = GetActorTransform();
+	
+	FVector Location = Transform.GetLocation();
+	FRotator Rotator = Transform.Rotator();
+	FVector Scale = Transform.GetScale3D();
+
+	UE_LOG(LogBaseGeometry,Warning,TEXT("Actor Name:%s"),*GetName());
+	UE_LOG(LogBaseGeometry,Warning,TEXT("Transform:%s"),*Transform.ToString());
+	UE_LOG(LogBaseGeometry,Warning,TEXT("Location:%s"),*Location.ToString());
+	UE_LOG(LogBaseGeometry,Warning,TEXT("Rotator:%s"),*Rotator.ToString());
+	UE_LOG(LogBaseGeometry,Warning,TEXT("Scale:%s"),*Scale.ToString());
+	
+	UE_LOG(LogBaseGeometry,Error,TEXT("Transform:%s"),*Transform.ToHumanReadableString());
+}
--- Source/GeometrySandbox/Public/BaseGeometryActor.h
+++ Source/GeometrySandbox/Public/BaseGeometryActor.h
@@ -15,12 +15,47 @@ public:
 	// Sets default values for this actor's properties
 	ABaseGeometryActor();
 
+	//组件
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* BaseMesh;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	//变换
+	UPROPERTY(EditAnywhere,Category="Movement")
+	float Amplitude = 50.0f;	
+
+	UPROPERTY(EditAnywhere,Category="Movement")
+	float Frequency = 2.0f;
+	
+	//变量
+	UPROPERTY(EditAnywhere,Category="Weapon")
+	int32 WeaponsNum = 4;
+
+	UPROPERTY(EditDefaultsOnly,Category="Stat")
+	int32 KillsNum = 7;
+
+	UPROPERTY(EditInstanceOnly,Category="Health")
+	float Health = 34.1235;
+
+	UPROPERTY(EditAnywhere,Category="Health")
+	bool IsDead = false;
+
+	UPROPERTY(VisibleAnywhere,Category="Weapon")
+	bool HasWeapon = true;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
+private:
+	//位置	
+	FVector InitialLocation;
+	//字符串
+	void printStringTypes();
+	//日志
+	void printTypes();
+	//变换
+	void printTransform();
 };
